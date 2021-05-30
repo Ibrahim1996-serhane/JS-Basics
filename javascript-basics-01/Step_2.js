31f12cad5fe84d071785f6847f5fd0770739d725
ibrahim1996-serhane <ibrahim.serhane1996@gmail.com>
2021-05-30 17:20:32 +0300

    Step 2 - Variables

---
+++ javascript-basics-01/Step_2.js
@@ -0,0 +1,10 @@
+
+
+function showAlert() {
+var name = "bob";
+var surname = "Serhane";
+var city = "Beirut";
+  alert("name:  " +name +"\n"+
+  "surname:  " + surname +"\n"+
+"city:  " + city);
+}
