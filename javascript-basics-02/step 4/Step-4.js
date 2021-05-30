8eab03635d58d8ca94d6d5bf4ef7f8fe733a3748
ibrahim1996-serhane <ibrahim.serhane1996@gmail.com>
2021-05-30 23:03:38 +0300

    step 2.4

--- javascript-basics-02/step 4/Step-4.js
+++ javascript-basics-02/step 4/Step-4.js
@@ -1,2 +1,13 @@
 
+btn_reset.addEventListener('click', () => {
+    let name = document.querySelector('#name');
+    let surname = document.querySelector('#surname');
+    let city = document.querySelector('#city');
+    let reset = document.querySelector('#reset');
+    if(confirm("press enter to reset the info")){
+        name.value = '';
+        surname.value = '';
+        city.value = '';
+    }
+});
 
