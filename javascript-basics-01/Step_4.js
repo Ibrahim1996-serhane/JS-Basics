ff39e1d5e6b24094812d6268167d896bb98aaa4e
ibrahim1996-serhane <ibrahim.serhane1996@gmail.com>
2021-05-30 18:05:40 +0300

    step -4 strings concatenation 2

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,8 @@
+
+    var name = prompt("Please enter your name:");
+    var surname = prompt("Please enter your surname:");
+    var city = prompt("Please enter your city:");
+    alert("name: " + name + "\nsurname: " + surname + "\ncity: " + city);
+
+
+
