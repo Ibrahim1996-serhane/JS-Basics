bf11c30132801a2e92459f34ee36d8d051d02cac
ibrahim1996-serhane <ibrahim.serhane1996@gmail.com>
2021-05-30 23:32:38 +0300

    step 2.6

---
+++ javascript-basics-02/step 6/Step_6.js
@@ -0,0 +1,40 @@
+let img_1= document.querySelector('#image1');
+let img_2= document.querySelector('#image2');
+let img_3= document.querySelector('#image3');
+let img_4 = document.querySelector('#image4');
+let img_5= document.querySelector('#image5');
+
+document.addEventListener('mouseover', event => {
+  if (event.target == img_1 ) {
+    img_1.src = "images/image1_2.jpg";
+  }
+  else if(event.target==img_2){
+    img_2.src="images/image2_2.jpg";
+  }
+  else if(event.target==img_3){
+    img_3.src="images/image3_2.jpg";
+  }
+  else if(event.target==img_4){
+    img_4.src="images/image4_2.jpg";
+  }
+  else if(event.target==img_5){
+    img_5.src="images/image5_2.jpg";
+  }
+})
+document.addEventListener('mouseout', event =>{
+    if (event.target == img_1 ) {
+        img_1.src = "images/image1.jpg";
+      }
+      else if(event.target==img_2){
+        img_2.src="images/image2.jpg";
+      }
+      else if(event.target==img_3){
+        img_3.src="images/image3.jpg";
+      }
+      else if(event.target==img_4){
+        img_4.src="images/image4.jpg";
+      }
+      else if(event.target==img_5){
+        img_5.src="images/image5.jpg";
+      }
+    })
