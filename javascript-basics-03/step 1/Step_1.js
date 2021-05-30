d234ef9b9f597b7121f62b3213dc9551b4254eff
ibrahim1996-serhane <ibrahim.serhane1996@gmail.com>
2021-05-31 00:20:36 +0300

    step 3.1

---
+++ javascript-basics-03/step 1/Step_1.js
@@ -0,0 +1,12 @@
+let image_1 = document.querySelector('#image1');
+image_1.addEventListener('mouseover',() => {
+    image_1.style.border = '3px solid red';
+})
+image_1.addEventListener('mouseleave',()=>{
+    image_1.style.border="none";
+});
+
+
+
+
+
