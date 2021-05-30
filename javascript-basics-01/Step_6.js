237f82c192e463d22306949f8a7269462cc70ef2
ibrahim1996-serhane <ibrahim.serhane1996@gmail.com>
2021-05-30 19:12:57 +0300

    step -6 external files and simple math

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,9 @@
+
+function divide() {
+    var first_number  = parseFloat(document.getElementById('first_number').value);
+    var second_number = parseFloat(document.getElementById('second_number').value);
+    var result =first_number / second_number;
+    // document.getElementById("demo").innerHTML = result ;
+    alert("result:  " +result +"\n");
+    return first_number / second_number ;
+}
