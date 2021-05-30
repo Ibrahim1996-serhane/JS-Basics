c58ee65e40f16835adaa0c7bc202b2466f0d82c3
ibrahim1996-serhane <ibrahim.serhane1996@gmail.com>
2021-05-30 19:00:08 +0300

    step -5 simple math

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,8 @@
+
+      function multiply() {
+        var first_number  = parseFloat(document.getElementById('first_number').value);
+        var second_number = parseFloat(document.getElementById('second_number').value);
+        var result =first_number * second_number;
+        // document.getElementById("demo").innerHTML = result ;
+        alert("result:  " +result +"\n");
+    }
